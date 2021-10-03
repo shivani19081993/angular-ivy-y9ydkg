b27e8d026fb59309e84882845eecab427dcd2bb4
shivani19081993 <guptashivani1908@gmail.com>
2021-10-03 15:06:07 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  candidates = [
+    { id: 1, first_name: '', last_name: '', email: '', gender: '' },
+  ];
 }
+/*var app = angular.module('plunker', []);
+app.controller('MainCtrl', function ($scope) {
+  $scope.candidates = [
+    { id: 1, first_name: '', last_name: '', email: '', gender: '' },
+  ];
+});*/
